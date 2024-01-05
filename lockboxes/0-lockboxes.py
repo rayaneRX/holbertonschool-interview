de06aa28f6dc042d64cbe2000ec138c9958136d3
rayaneRX <rayane@rayane>
2024-01-05 13:27:39 +0100

    task 0

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
+     # Get the total number of boxes
     n = len(boxes)
     visited = [False] * n
     stack = [0]
@@ -12,5 +13,5 @@ def canUnlockAll(boxes):
         for key in boxes[current_box]:
             if not visited[key]:
                 stack.append(key)
-
+# Check if all boxes have been visited
     return all(visited)
