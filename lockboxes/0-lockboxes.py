8f5f0959c61303b834e3fb00a5594168667e6189
rayaneRX <rayane@rayane>
2024-01-05 13:39:16 +0100

    task 0

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,16 +3,15 @@
 a method that determines if all the boxes can be opened.
 """
 def canUnlockAll(boxes):
-    n = len(boxes)
-    visited = [False] * n
-    stack = [0]
+    visited = set()
 
-    while stack:
-        current_box = stack.pop()
-        visited[current_box] = True
+    def dfs(box_index):
+        visited.add(box_index)
 
-        for key in boxes[current_box]:
-            if not visited[key]:
-                stack.append(key)
+        for key in boxes[box_index]:
+            if key < len(boxes) and key not in visited:
+                dfs(key)
 
-    return all(visited)
+    dfs(0)
+
+    return len(visited) == len(boxes)
