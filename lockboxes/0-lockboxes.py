bac1261c32590150e3da2b923f38534b252ec827
rayaneRX <rayane@rayane>
2024-01-05 13:32:41 +0100

    task 0

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
+"""
+a method that determines if all the boxes can be opened.
+"""
 def canUnlockAll(boxes):
-     # Get the total number of boxes
     n = len(boxes)
     visited = [False] * n
     stack = [0]
-#Lockboxes
+
     while stack:
         current_box = stack.pop()
         visited[current_box] = True
@@ -13,5 +14,5 @@ def canUnlockAll(boxes):
         for key in boxes[current_box]:
             if not visited[key]:
                 stack.append(key)
-# Check if all boxes have been visited
+
     return all(visited)
