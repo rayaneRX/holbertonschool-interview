27cebdedc5644dd56da9593a0dc8060532f37f96
rayaneRX <rayane@rayane>
2024-01-12 17:07:33 +0100

    task 0

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - inserts a number into a sorted listint_t linked list
+ * @head: pointer to the head of the list
+ * @number: integer to insert into the list
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new_node, *current, *prev;
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = number;
+    new_node->next = NULL;
+
+    current = *head;
+    prev = NULL;
+
+    while (current != NULL && current->n < number)
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    if (prev == NULL)
+    {
+        new_node->next = *head;
+        *head = new_node;
+    }
+    else
+    {
+        prev->next = new_node;
+        new_node->next = current;
+    }
+
+    return (new_node);
+}
