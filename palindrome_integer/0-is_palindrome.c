bd1d55d3a711a194ad12940e814e3218df920efa
rayaneRX <rayane@rayane>
2024-03-01 17:27:21 +0100

    task 0

--- palindrome_integer/0-is_palindrome.c
+++ palindrome_integer/0-is_palindrome.c
@@ -1,20 +1,14 @@
 #include "palindrome.h"
 
-/**
- * reverse_number - Function to reverse a number
- * @n: The number to reverse
- *
- * Return: The reversed number
- */
-unsigned long reverse_number(unsigned long n)
+int is_palindrome(unsigned long n)
 {
-    unsigned long reversed = 0;
+	unsigned long reversed  = 0, original = n;
 
-    while (n != 0)
-    {
-        reversed = reversed * 10 + n % 10;
-        n = n / 10;
-    }
+	while (n != 0)
+	{
+		reversed = reversed * 10 + n % 10;
+		n /= 10;
+	}
 
-    return reversed;
+	return (original == reversed ? 1 : 0);
 }
