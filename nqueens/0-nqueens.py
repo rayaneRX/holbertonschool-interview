e63324ef5d93700aebc3f0c0e6e28a08e503e6a2
rayaneRX <rayane@rayane>
2024-04-05 16:14:22 +0200

    fix

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -55,6 +55,10 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         sys.exit(1)
     
+    if sys.argv[1] == '':
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
     try:
         N = int(sys.argv[1])
         solve_nqueens(N)
