9242436ea7eedfe67f64988a5a3733f408b590a3
rayaneRX <rayane@rayane>
2024-04-26 23:53:57 +0200

    task 0

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -2,45 +2,36 @@
 
 const request = require('request');
 const movieId = process.argv[2];
+
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-async function fetchCharacterNames(urls) {
-    const characterNames = [];
-    for (const url of urls) {
-        try {
-            const characterData = await fetchData(url);
-            characterNames.push(characterData.name);
-        } catch (error) {
-            console.error(`Error fetching character data: ${error}`);
-        }
-    }
-    return characterNames;
-}
-
-function fetchData(url) {
-    return new Promise((resolve, reject) => {
-        request(url, { json: true }, (error, response, body) => {
-            if (error) {
-                reject(error);
-            } else if (response.statusCode !== 200) {
-                reject(new Error(`Failed to get data: ${response.statusCode}`));
-            } else {
-                resolve(body);
-            }
-        });
+function getCharacterName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, { json: true }, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(new Error(`Failed to get character data: ${response.statusCode}`));
+      } else {
+        resolve(body.name);
+      }
     });
+  });
 }
 
 request(url, { json: true }, async (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-        console.error(`Error fetching movie data: ${error || response.statusCode}`);
-        return;
-    }
+  if (error) { return console.log(error); }
+  if (response.statusCode !== 200) {
+    return console.log(`Failed to get film data: ${response.statusCode}`);
+  }
 
-    try {
-        const characterNames = await fetchCharacterNames(body.characters);
-        characterNames.forEach(name => console.log(name));
-    } catch (error) {
-        console.error(`Error fetching character names: ${error}`);
-    }
+  const characterUrls = body.characters;
+  const characterPromises = characterUrls.map(url => getCharacterName(url));
+
+  try {
+    const characterNames = await Promise.all(characterPromises);
+    characterNames.forEach(name => console.log(name));
+  } catch (error) {
+    console.log(error);
+  }
 });
