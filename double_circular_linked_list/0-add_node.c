727b2ef140204388352d6e2b51610034b4d3120e
rayaneRX <rayane@rayane>
2024-07-19 15:03:19 +0200

    fix Betty

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -13,7 +13,7 @@ List *add_node_end(List **list, char *str)
 {
     List *new_node, *last;
 
-    new_node = malloc(sizeof(List));
+		new_node = malloc(sizeof(List));
     if (!new_node)
         return (NULL);
 
