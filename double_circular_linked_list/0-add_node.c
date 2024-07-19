272f85e08c9c96f7549c9d47243d6440f727470e
rayaneRX <rayane@rayane>
2024-07-19 15:09:20 +0200

    fix Betty

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -6,7 +6,7 @@
  * add_node_end - Add a new node to the end of a double circular linked list
  * @list: the list to modify
  * @str: the string to copy into the new node
- * 
+ *
  * Return: Address of the new node, or NULL on failure
  */
 List *add_node_end(List **list, char *str)
@@ -43,10 +43,10 @@ List *add_node_end(List **list, char *str)
 }
 
 /**
- * add_node_begin - Add a new node to the beginning of a double circular linked list
+ * add_node_begin - Add a new node to the beginning
  * @list: the list to modify
  * @str: the string to copy into the new node
- * 
+ *
  * Return: Address of the new node, or NULL on failure
  */
 List *add_node_begin(List **list, char *str)
